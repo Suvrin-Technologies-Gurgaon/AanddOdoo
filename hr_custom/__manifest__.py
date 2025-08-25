edb13b3c57e0105c2a7e075d73c4ffc3df94c640
vishal <vishal.chaudhary@suvrin.com>
2025-08-25 06:11:39 +0000

    Add hr custom module

---
+++ hr_custom/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "HR Custom",
+    "author": "Customistaion in HR Modules",
+    "category": "HR",
+    "license": "OPL-1",
+    "summary": "Customistaion in HR Modules",
+    "description": """Customistaion in HR Modules""",
+    "version": "18.0.0.1",
+    "depends": ["base", "hr","hr_holidays"],
+    "application": True,
+    "data": [
+        'security/security.xml',
+    ],
+    "images": [ ],
+    "auto_install": False,
+    "installable": True,
+}
